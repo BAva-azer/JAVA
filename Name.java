a002f04185b5b3164e8259e668915a5d5a28e840
BAva-azer <bavashreejayachandran2005@gmail.com>
2026-06-18 23:28:12 +0530

    Add files via upload

---
+++ Name.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class Main{
+ public static void main (String[] parameter){
+ Scanner kbrd = new Scanner(System.in);
+ String name = kbrd.nextLine();
+ System.out.println("MY Name is:"+ name);
+ }
+}
