e1238d88decd50e665f79ebc1445ef256c366d07
BAva-azer <bavashreejayachandran2005@gmail.com>
2026-06-18 23:30:59 +0530

    ageofperson

---
+++ Age.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class Age{
+ public static void main (String[] parameter){
+ Scanner kbrd = new Scanner(System.in);
+ int Age = kbrd.nextInt();
+ System.out.println("I am "+ Age +" years young");
+ }
+}
