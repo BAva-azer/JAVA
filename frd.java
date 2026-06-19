f51eff4d0f27598e419370690fed29349cd33fb5
BAva-azer <bavashreejayachandran2005@gmail.com>
2026-06-19 23:50:30 +0530

    Add files via upload

---
+++ frd.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class frd{
+    public static void main(String[]parameter){
+    Scanner kbrd = new Scanner(System.in);
+    String name = kbrd.nextLine();
+    String fname = kbrd.nextLine();
+    System.out.println("I am " + name + " and my best frnd is " + fname );
+    
+    }
+}
