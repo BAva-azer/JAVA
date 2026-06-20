263917d9ec5c6dfa7e989defb9386a9a9e456d05
Bavashree Jayachandran <bavashreejayachandran2005@gmail.com>
2026-06-20 23:51:10 +0530

    evennum

---
+++ even.java
@@ -0,0 +1,16 @@
+
+
+class even{
+    public static void main(String args [])
+    {
+        int num = 13;
+        if(num%2 ==0)
+        {
+        System.out.println("even number");
+        }
+    else{
+        System.out.println("not even");
+    }
+
+    }
+}
