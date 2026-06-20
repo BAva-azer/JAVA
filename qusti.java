9bd44dc1c7b2e946382d90f32a1e3fe53fe81f90
Bavashree Jayachandran <bavashreejayachandran2005@gmail.com>
2026-06-20 23:58:07 +0530

    comparison

---
+++ qusti.java
@@ -0,0 +1,17 @@
+class qusti{
+    public static void main(String args[])
+    {
+      int score = 75;
+      if(score<50)
+      {
+        System.out.println("you need to improve");
+      } 
+      else if(score>=50 && score<=70)
+      {
+        System.out.println("good job");
+      } 
+      else{
+        System.out.println("excellent");
+      }
+    }
+}
